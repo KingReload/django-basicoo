26aaee52dbadac0a72652e4299dc4c74977ed6f5
Wesley <w.arends@hotmail.com>
2018-04-30 12:21:42 +0200

    removed the jquery-1.12.1 version and import it through the website, but now the datatables won't run if you don't have internet

--- webpack.config.js
+++ webpack.config.js
@@ -86,7 +86,6 @@ module.exports = [
           jQuery: "jquery"
       }),
       new CopyWebpackPlugin([
-        { from: 'frontend/js/jquery/jquery-1.12.1.js', to: 'jquery' },
         { from: 'frontend/js/datatables/datatables-1.10.16.js', to: 'datatables' },
         { from: 'frontend/js/datatable-control.js', to: '' },
         { from: 'frontend/css/', to: '../css' },
